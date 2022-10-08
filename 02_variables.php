40cb9ed5fcc56bbc97c729abc737ec91596d7497
Mohamed Musa <mohamed.musa831@gmail.com>
2022-10-08 13:56:22 +0200

    first commit

---
+++ 02_variables.php
@@ -0,0 +1,31 @@
+<?php
+/* ----------PHP Data Types------------ */
+/*
+
+- STRING      Series of characters surrounded by quotes
+- INTERGER    Whole numbers
+- FLOAT       Decimal numbers
+- BOOLEAN     True or false
+- ARRAY       Special variables which can hold more than value
+- OBJECT      A class
+- NULL        Empty variable
+- RESOURCE    Special variable that holds a resource
+
+*/
+
+/* ----------VARIABLES RULES ------------ */
+
+/*
+- Must be prefixed by $
+- Must start with letter or underscore character
+- Can't start with a number
+- Variables are case-sensetive( $name $NAME not the same variable)
+
+*/
+
+$name = 'Iyad'; // String
+$age = 3; // Int
+$goes_to_school = true; // bool
+$cash_on_hand = 13.95; // float
+
+echo $goes_to_school;
