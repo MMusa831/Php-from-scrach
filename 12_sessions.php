681e9f062a6d0e80c58cb997a2bd3e7ad76e5c74
Mohamed Musa <mohamed.musa831@gmail.com>
2022-10-10 16:39:41 +0200

    Sessions

---
+++ 12_sessions.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+  Sessions are a way to store information (in variables)
+   to be used across multiple pages. Unlike cookies,
+    sessions are stored on the server.
+*/
+session_start();
+
+if(isset($_POST['submit'])) {
+    $username = filter_input(INPUT_POST, 'username',
+    FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = $_POST['password'];
+}
+ if ($username == 'iyad' && $password == 'password') {
+    // Set Session variable
+    $_SESSION['username'] = $username;
+    // Redirect user to another page
+    header('Location: /extras/dashboard.php');
+  } else {
+    echo 'Incorrect username or password';
+  }
+?>
+
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+    <div>
+        <label for="username">Username: </label>
+        <input type="text" name="username">
+    </div>
+    <div>
+        <label for="password">Password: </label>
+        <input type="password" name="password">
+    </div>
+    <input type="submit" value="Submit" name="submit">
+</form>
