66c258b4fca9fb212f7d6878ccd70d8e6bb080c6
Mohamed Musa <mohamed.musa831@gmail.com>
2022-10-10 15:12:11 +0200

    GET POST

---
+++ 10_get_post.php
@@ -0,0 +1,30 @@
+<?php
+/* -----GET and POST supergalobals----*/
+
+/* 
+
+we can pass data through urls and forms using the $_GET and
+ $_POST superglobals.
+
+ */
+
+     echo $_POST['name'];
+     echo $_POST['age']
+?>
+
+
+<a href="<?php echo $_SERVER['PHP_SELF']; ?>?name=iyad&age=30">
+    Click
+</a>
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+    <div>
+        <label for="name">Name: </label>
+        <input type="text" name="name">
+    </div>
+    <div>
+        <label for="age">Age: </label>
+        <input type="text" name="age">
+    </div>
+    <input type="submit" value="Submit" name="submit">
+</form>
